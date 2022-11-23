f96762f2c5b79513b78c631be30d441f0c295855
Artem Druganov <104306822+Mazilas2@users.noreply.github.com>
2022-11-23 13:57:00 +0500

    Delete ReadRev.py

--- ReadRev.py
+++
@@ -1,9 +0,0 @@
-import docx
-
-def ReadOtcRev(filedir):
-    # Read docx file at filedir (only text)
-    doc = docx.Document(filedir)
-    fullText = []
-    for para in doc.paragraphs:
-        fullText.append(para.text)
-    return fullText
